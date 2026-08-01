bb8cc54827830885934da239acc01419a3de798f
Santhosh Kumar K <144673583+santhosh9455@users.noreply.github.com>
2026-08-01 10:45:00 +0530

    chore: remove package.json and package-lock.json dependency configuration files

--- src/app/auth-guard.ts
+++
@@ -1,9 +0,0 @@
-import { inject } from '@angular/core';
-import { CanActivateFn, Router } from '@angular/router';
-
-
-export const authGuard: CanActivateFn = (route, state) => {
- const token = sessionStorage.getItem('authToken');
-  if (token) return true;
-  return inject(Router).navigate(['/login']).then(() => false);
-};
